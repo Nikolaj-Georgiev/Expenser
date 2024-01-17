145e67871d6b82a9eeea2fec9077ebbaa5a7446c
NGNikolay <georgiev.nikolaj@gmail.com>
2024-01-17 23:09:49 +0200

    styling components

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -16,8 +16,8 @@ export default function Button({ children, isCta, isText, ...props }) {
   return (
     <motion.button
       className={cssClasses}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.8 }}
+      whileHover={{ scale: 1.08 }}
+      whileTap={{ scale: 0.85 }}
       {...props}
     >
       {children}
