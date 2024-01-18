8c2df9b53c873f417a9f38626426511a717e492b
NGNikolay <georgiev.nikolaj@gmail.com>
2024-01-18 12:22:52 +0200

    edited styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import classes from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={classes.container}>
-      <p>Footer</p>
+      <p>All rights reserved &copy; 2024 GNikolay </p>
+      <img />
     </footer>
   );
 }
