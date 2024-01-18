ea742958a78253fb4a7c2e48902eee36d4d05dec
NGNikolay <georgiev.nikolaj@gmail.com>
2024-01-18 12:15:58 +0200

    edited styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import classes from './Footer.module.css';
 
 export default function Footer() {
   return (
-    <div className={classes.container}>
+    <footer className={classes.container}>
       <p>Footer</p>
-    </div>
+    </footer>
   );
 }
