9892e824a027aa1a2eba114480d950bc214a069b
NGNikolay <georgiev.nikolaj@gmail.com>
2024-01-27 09:33:20 +0200

    fixed animation in Testimonials section

--- src/components/testimonials/TestimonialsSection.jsx
+++ src/components/testimonials/TestimonialsSection.jsx
@@ -1,21 +1,74 @@
+import { motion } from 'framer-motion';
+
 import classes from './TestimonialsSection.module.css';
 import chartSmall from '../../assets/customers/testimonials-chart-small.webp';
 import { CUSTOMERS } from '../../util/config.js';
 import Testimonial from './Testimonial.jsx';
 
 export default function TestimonialsPage() {
+  const list = {
+    visible: {
+      opacity: 1,
+      transition: {
+        when: 'beforeChildren',
+        staggerChildren: 0.2,
+      },
+      x: 0,
+    },
+    hidden: {
+      opacity: 0,
+      transition: { when: 'afterChildren' },
+    },
+  };
+
+  const left = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      duration: 0.4,
+    },
+    hidden: {
+      opacity: 0,
+      x: -300,
+    },
+  };
+
+  const right = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      duration: 0.4,
+    },
+    hidden: {
+      opacity: 0,
+      x: 300,
+    },
+  };
+
   return (
     <>
       <section
         className={classes['testimonials-section']}
         id='testimonials'
       >
-        <div className={classes['section-container']}>
-          <h2 className={classes.heading}>
+        <motion.div
+          className={classes['section-container']}
+          initial='hidden'
+          whileInView='visible'
+          // animate='visible'
+          variants={list}
+        >
+          <motion.h2
+            className={classes.heading}
+            variants={left}
+          >
             Ones you try it, you can&apos;t go back!
-          </h2>
+          </motion.h2>
           <div className={classes.container}>
-            <div className={classes.img}>
+            <motion.div
+              className={classes.img}
+              variants={left}
+            >
               {/* <picture>
                 <source
                   srcSet={chartSmall}
@@ -32,10 +85,13 @@ export default function TestimonialsPage() {
                 className={classes.img}
               />
               {/* </picture> */}
-            </div>
+            </motion.div>
 
             <div>
-              <div className={classes.testimonials}>
+              <motion.div
+                className={classes.testimonials}
+                variants={right}
+              >
                 {CUSTOMERS.map((customer) => (
                   <Testimonial
                     key={customer.id}
@@ -45,10 +101,10 @@ export default function TestimonialsPage() {
                     testimonial={customer.testimonial}
                   />
                 ))}
-              </div>
+              </motion.div>
             </div>
           </div>
-        </div>
+        </motion.div>
       </section>
     </>
   );
